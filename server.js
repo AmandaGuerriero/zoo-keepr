745a236850a06a6588751c5a995f18218681a8cf
Amanda Guerriero <ag.codingclass@gmail.com>
2020-10-14 18:54:17 -0700

    connected frontend

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// Make files available
+app.use(express.static('public'));
 
 // Filter By 
 function filterByQuery(query, animalsArray) {
@@ -117,6 +119,18 @@ app.post('/api/animals', (req, res) => {
 
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 
 app.listen(PORT, () => {
